2ee5752ee46e520e276bfbb372e2f9a9940ac83d
Julian Cosmeu <jcosme1@ggc.edu>
2024-02-07 15:30:10 -0500

    init

---
+++ src/InheritanceDemo/Raptile.java
@@ -0,0 +1,18 @@
+package InheritanceDemo;
+
+
+
+public class Raptile extends Animal{
+    private String color;
+    private boolean hasStrips;
+    private int numOfLegs;
+
+    public Reptile(String color, boolean hasStrips, int numOfLegs){
+        super(true,0,"Carnivore");
+
+    }
+
+   public Reptile(){
+
+   }
+}
